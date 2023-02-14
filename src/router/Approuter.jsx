11f8730995ae9239755dc7436286f23064568a8c
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-14 00:06:20 -0300

    routes

--- src/router/Approuter.jsx
+++ src/router/Approuter.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import { ChatPage } from '../pages/ChatPage'
+import { LoginPage } from '../pages/LoginPage'
+import { RegisterPage } from '../pages/RegisterPage'
 
 export const Approuter = () => {
     return (
-        <div>Approuter</div>
+        <Routes>
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/register' element={<RegisterPage />} />
+            <Route path='/' element={<ChatPage />} />
+        </Routes>
     )
 }
