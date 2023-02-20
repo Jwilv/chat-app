de93e94b0914e7cf3277d6c4c06b876298465306
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-20 18:18:15 -0300

    SocketProvider

--- src/ChatApp.jsx
+++ src/ChatApp.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter } from "react-router-dom"
 import { AuthProvider } from "./auth/AuthContext"
+import { SocketProvider } from "./context/SocketContext"
 import { Approuter } from "./router/Approuter"
 
 function ChatApp() {
 
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Approuter />
-      </BrowserRouter>
+      <SocketProvider>
+        <BrowserRouter>
+          <Approuter />
+        </BrowserRouter>
+      </SocketProvider>
     </AuthProvider>
 
   )
