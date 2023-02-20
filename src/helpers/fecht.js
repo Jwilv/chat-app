79c120e468509254371bd6134aed564bbec86a8c
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-20 15:06:36 -0300

    fetchToken

--- src/helpers/fecht.js
+++ src/helpers/fecht.js
@@ -17,9 +17,38 @@ export const fetchWithoToken = async( endpoint, data, method = 'GET' ) => {
             body: JSON.stringify( data )
         })
 
-        const body = await resp.json();
+        
 
-        return await body
+        return await resp.json();
+    }
+
+}
+
+export const fetchToken = async( endpoint, data, method = 'GET' ) => {
+
+    const url = `${ baseUrl }/${ endpoint }`;
+    const token = localStorage.getItem('token') || '';
+
+    if ( method === 'GET' ) {
+        const resp = await fetch( url, {
+            headers:{
+                'x-token':token,
+            }
+        });
+        return await resp.json();
+    } else {
+        const resp = await fetch( url, {
+            method : method,
+            headers: {
+                'Content-type': 'application/json',
+                'x-token' : token,
+            },
+            body: JSON.stringify( data )
+        })
+
+        
+
+        return await resp.json();
     }
 
 }
