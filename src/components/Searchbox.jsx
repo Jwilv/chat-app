bc3d343a9de8b0f16853af71f775c2ce1d4415c9
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-20 17:33:44 -0300

    logout button

--- src/components/Searchbox.jsx
+++ src/components/Searchbox.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../auth/AuthContext';
 
 export const Searchbox = () => {
 
-    // const { auth, logout } = useContext( AuthContext );
+    const { auth, logout } = useContext( AuthContext );
 
     return (
         <div className="headind_srch">
             <div className="recent_heading mt-2">
-                {/* <h4>{ auth.name }</h4> */}
-                <h4>juan</h4>
+                <h4>{ auth.name }</h4> 
             </div>
             <div className="srch_bar">
                 <div className="stylish-input-group">
                     <button 
                         className="btn text-danger"
-                        // onClick={ logout }
+                        onClick={ logout }
                     >
                         Salir
                     </button>
