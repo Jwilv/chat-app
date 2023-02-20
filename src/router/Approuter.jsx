1b9196b133ef65cc65025d3008f6e81ec027a833
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-20 17:22:36 -0300

    integracion de ruta privadas y publicas

--- src/router/Approuter.jsx
+++ src/router/Approuter.jsx
@@ -3,23 +3,36 @@ import { Route, Routes } from 'react-router'
 import { AuthContext } from '../auth/AuthContext'
 import { ChatPage } from '../pages/ChatPage'
 import { AuthRouter } from './AuthRouter'
+import { PrivateRouter } from './PrivateRouter'
+import { PublicRouter } from './PublicRouter'
 
 export const Approuter = () => {
 
-    const {auth, validateToken} = useContext(AuthContext);
+    const { auth, validateToken } = useContext(AuthContext);
 
-    useEffect( ()=>{
+    useEffect(() => {
         validateToken();
-    },[])
+    }, [])
 
-    if(auth.checking){
+    if (auth.checking) {
         return <h1>espere</h1>
     }
 
     return (
         <Routes>
-            <Route path='/auth/*' element={<AuthRouter />} />
-            <Route path='/' element={<ChatPage />} />
+
+            <Route path='/auth/*' element={
+                <PublicRouter logged={auth.logged}>
+                    <AuthRouter />
+                </PublicRouter>
+            } />
+
+            <Route path='/' element={
+                <PrivateRouter logged={auth.logged}>
+                    <ChatPage />
+                </PrivateRouter>
+            } />
+
         </Routes>
     )
 }
