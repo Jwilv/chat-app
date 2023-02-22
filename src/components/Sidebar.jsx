7e9f7de9b516be2c95c37ffed73dcbf26489d6c9
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-22 17:14:44 -0300

    chatState Siderbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ChatContext } from '../context/chat/ChatContext'
 import { SidebarChatItem } from './SidebarChatItem'
 
 export const Sidebar = () => {
 
-    const chats = [1,2,3,4,5,6,7,8,9,10]
-
+    const {chatState} = useContext(ChatContext);
+    const {users} = chatState
 
     return (
         <div className="inbox_chat">
 
             {
-                chats.map( (chat) => (
-                    <SidebarChatItem key={ chat } />
+                users.map( (user) => (
+                    <SidebarChatItem 
+                    key={ user.id }
+                    user={user}
+                    />
                 ))
             }
 
