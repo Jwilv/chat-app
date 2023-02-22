c7315148d73dcc6726b9966bbaf3ac84901cfd1b
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-22 17:19:44 -0300

    user en SidebarChatItem

--- src/components/SidebarChatItem.jsx
+++ src/components/SidebarChatItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const SidebarChatItem = () => {
+export const SidebarChatItem = ({user}) => {
     return (
         <div className="chat_list">
             {/* active_chat */}
@@ -9,9 +9,12 @@ export const SidebarChatItem = () => {
                     <img src="https://p.kindpng.com/picc/s/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png" alt="sunil" />
                 </div>
                 <div className="chat_ib">
-                    <h5>Some random name</h5>
-                    <span className="text-success">Online</span>
-                    <span className="text-danger">Offline</span>
+                    <h5>{user.name}</h5>
+                    {
+                        (user.online)
+                        ? <span className="text-success">Online</span>
+                        : <span className="text-danger">Offline</span>
+                    }
                 </div>
             </div>
         </div>
