c99971869dcf48b8364fb2f4cfdc663693f8038d
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-23 13:57:47 -0300

    case setActive

--- src/context/chat/ChatReducer.js
+++ src/context/chat/ChatReducer.js
@@ -3,10 +3,22 @@
 export const ChatReducer = (state, action) => {
     switch (action.type) {
         case 'getUsers':
-            return{
+            return {
                 ...state,
-                users:[...action.payload]
+                users: [...action.payload]
             }
+        case 'setActive':
+
+            if (state.chatActivo === action.payload) {
+                return state;
+            }
+
+            return {
+                ...state,
+                chatActivo: action.payload,
+            }
+
+            
         default:
             return state;
     }
