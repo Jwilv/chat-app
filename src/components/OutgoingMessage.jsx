3bcac06f4a3529452c278f7808d2f6ecf45f8c9e
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-24 21:31:05 -0300

    hourMonth in OutgoingMessage

--- src/components/OutgoingMessage.jsx
+++ src/components/OutgoingMessage.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { hourMonth } from '../helpers/hourMonth'
 
 export const OutgoingMessage = ({message}) => {
+
+    const date = hourMonth(message.createdAt)
     return (
         <div className="outgoing_msg">
             <div className="sent_msg">
                 <p>{message.message}</p>
-                <span className="time_date"> 11:01 AM | June 9</span>
+                <span className="time_date">{date}</span>
             </div>
         </div>
     )
