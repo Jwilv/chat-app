5c51d0abe3170b24d65cedcb5704d6a14898c795
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-25 16:44:58 -0300

    handleLogout

--- src/components/Searchbox.jsx
+++ src/components/Searchbox.jsx
@@ -1,9 +1,18 @@
 import React, { useContext } from 'react'
 import { AuthContext } from '../auth/AuthContext';
+import { ChatContext } from '../context/chat/ChatContext';
 
 export const Searchbox = () => {
 
     const { auth, logout } = useContext( AuthContext );
+    const {dispatch} = useContext(ChatContext);
+
+    const handleLogout = ()=>{
+        logout();
+        dispatch({
+            type:'logout'
+        })
+    }
 
     return (
         <div className="headind_srch">
@@ -14,7 +23,7 @@ export const Searchbox = () => {
                 <div className="stylish-input-group">
                     <button 
                         className="btn text-danger"
-                        onClick={ logout }
+                        onClick={ handleLogout }
                     >
                         Salir
                     </button>
